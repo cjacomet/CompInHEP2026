c50cc26bdb293199c45eb53c9042359fbd0c77ce
cjacomet <camille.jacomet+git@gmail.com>
2026-01-29 22:14:14 +0200

    question 3

--- Exercises/Ex2/returned_answers/CamilleJacomet/luminosity.py
+++ Exercises/Ex2/returned_answers/CamilleJacomet/luminosity.py
@@ -3,17 +3,15 @@ import re
 
 def read_lines(file):
     with open(file) as f:
-        lines = f.readlines()
-    
-    return lines
+        return f.readlines()
 
 
 def find_column(lines, colname):
     for i, line in enumerate(lines):
         # Match a pipe-separated header line
-        if re.search(rf"{re.escape(colname)}", line):
+        if re.search(rf"\b{colname}\b", line):
             headers = [h.strip() for h in line.split("|")]
-            col_index = headers.index(colname)
+            col_index = next(i for i, h in enumerate(headers) if colname in h)
             return i, col_index
     return None
 
@@ -24,8 +22,7 @@ def find_val(file, colname):
     if indices:
         line_index, col_index = indices
         
-        # Two rows below the header
-        data_line = lines[line_index + 2]
+        data_line = lines[line_index + 2] # Two rows below the header
         values = [v.strip() for v in data_line.split("|")]
 
         target_value = values[col_index]
@@ -33,7 +30,27 @@ def find_val(file, colname):
     return None
 
 
+def sum_vals(file, colname):
+    lines = read_lines(file)
+    indices = find_column(lines, colname)
+    if indices:
+        line_index, col_index = indices
+        line_index += 2 # Two rows below the header
+        sum = 0.0
+        while '|' in lines[line_index]:
+            data_line = lines[line_index]
+            values = [v.strip() for v in data_line.split("|")]
+            sum += float(values[col_index])
+            line_index += 1
+        return sum
+    return None
+
+
 if __name__=="__main__":
-    lum = find_val("brilcalc.log", "totrecorded(/pb)") #summary luminosity in /pb
-    lum *= 1e-3 #convert to /fb
+    lum = find_val("brilcalc.log", "totrecorded")
+    lum = round(lum * 1e-3, 1) #convert to /fb and round to 1 decimal
     print(f"totrecorded (/fb): {lum:.1f}")
+    sum_lum = sum_vals("brilcalc.log", "recorded")
+    sum_lum = round(sum_lum * 1e-3, 1) #convert to /fb and round to 1 decimal
+    print(f"sum of recorded (/fb): {sum_lum:.1f}")
+    print(f"sum recorded {'=' if lum == sum_lum else 'does not equal'} totrecorded")
