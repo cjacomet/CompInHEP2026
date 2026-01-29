b2420b839d381ec7eac86bb2a33af265f780ddfe
cjacomet <camille.jacomet+git@gmail.com>
2026-01-29 21:18:30 +0200

    bash script for parallel jobs + run make directly in python and bash scripts

--- Exercises/Ex2/returned_answers/CamilleJacomet/runJobs.py
+++ Exercises/Ex2/returned_answers/CamilleJacomet/runJobs.py
@@ -26,7 +26,8 @@ def wrapper(args):
 
 if __name__=="__main__":
     n = 10
-    input = random.sample(range(1,100), n)
+    input = random.sample(range(1,100), n) # Generate random input numbers
+    subprocess.run(["make"], check=True) # Compile and link helloNr
     with ProcessPoolExecutor(max_workers=n) as executor:
         executor.map(wrapper, zip(range(n), input))
     
---
+++ Exercises/Ex2/returned_answers/CamilleJacomet/runJobs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+make # Compile and link helloNr
+N=10 # Number of jobs
+
+for ((i=1; i<=N; i++)); do
+    RAND=$((RANDOM % 100 + 1)) # Generate random input number
+    make run ARG=$RAND > output_job${i}.txt &
+done
+
+wait
